23eb619d9ce65f28261d586901f7e4341ed85879
Alberto Perez <apo0106@gmail.com>
2023-12-19 11:07:17 +0100

    * added conditional in index.php that shows user logged in via session variable or login hyperlink * created login view with a form for introducing username & password and a hyperlink to register view * created loginController and registerController. Both empty for now

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+session_start();
+if(isset($_SESSION['username'])){
+    echo 'Welcome '.$_SESSION['username'];
+    echo '<br><a href="logout.php">Logout</a>';
+}else{
+    echo '<a href="/view/login.php">Login or register here!</a>';
+}
+?>  
+</body>
+</html>
